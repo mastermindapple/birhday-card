2050f4e339fd0192189edf6e08b3d537ad2a61d9
mastermindapple <56500380+mastermindapple@users.noreply.github.com>
2020-03-12 03:06:31 +0530

    Add files via upload

---
+++ Janav's Birthday/script.js
@@ -0,0 +1,11 @@
+function thankYou(){
+    document.getElementById("feedback").innerHTML = ("Thanks For Your Feedback!!");
+}
+
+function noWant(){
+    document.getElementById("feedback").innerHTML = ("No Problem, but please come in the party; Janav Would be Waiting.");
+}
+
+function trial(){
+    console.log("working good!");
+}
